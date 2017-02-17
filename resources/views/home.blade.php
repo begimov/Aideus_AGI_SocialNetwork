a7d12730269d93fe8f9ee5894a8aea361bac7040
begimov <begimov@gmail.com>
2017-02-17 17:24:44 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends('templates.default')
+
+@section('content')
+  <div class="container">
+        <h1 class="aideus-title">AGI Community</h1>
+        <p class="lead">The goal of our project is overcoming of restrictions of human thinking and solution of the most difficult and essential challenges and problems by creation of a strong artificial intelligence. We perfectly realize all complexity of the problem put by us and for this reason first of all we believe that it is necessary to ensure and support fundamental scientific research in this field.</p>
+        <a class="btn btn-default" href="#" role="button">Sign in</a> <a class="btn btn-default" href="#" role="button">Sign up</a>
+    <div class="row">
+      <div class="col-md-8">
+        <div class="aideus-icon">
+          <img src="" alt="" class="img-rounded" width="128px" height="128px">
+        </div>
+        <h2 class="aideus-left">It is important</h2>
+        <p class="aideus-desc">Human mentality essentially surpasses its biological needs, but possibilities of human mind are not boundless. First of all, limitation of intellectual possibilities is fully experienced by scientists. And although this limitation is less appreciable, it is very critical for the life of the whole human society. It concerns all problems of people – from traffic jams to economic crises and wars. In this connection we find particular interest in development of strong artificial intelligence technologies. Research in this field could not only can eventually lead to superhuman intelligence, not burdened by evolutionary ballast, but could also help us better understand our own thinking and to solve difficult scientific, technical, social and economic problems.</p>
+      </div>
+      <div class="col-md-4">
+        <div class="aideus-icon">
+          <img src="" alt="" class="img-rounded" width="128px" height="128px">
+        </div>
+        <h2 class="aideus-left">Your help</h2>
+        <p class="aideus-desc">Now your help and support is necessary for our project. If you really are excited with idea of creation of strong artificial intelligence if you consider this goal noble and want to change life of people to the best, please contact us, we are always glad to get acquainted with you!</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="aideus-icon">
+          <img src="" alt="" class="img-rounded" width="128px" height="128px">
+        </div>
+        <h2 class="aideus-left">Optimism</h2>
+        <p class="aideus-desc">Despite all complexity of the problem of creation of strong artificial intelligence, there are things which inspire our optimism and confidence in the possibility of implementation of our project. This is exponential development of information technologies, new discoveries providing further observance of the Moore's law, implementation of plans of achievement of human brain computing power by 2020's, progress in nanotechnologies, quantum computing, understanding of the principles of work of human brain and latest achievements in the field of AI.</p>
+      </div>
+      <div class="col-md-4">
+        <div class="aideus-icon">
+          <img src="" alt="" class="img-rounded" width="128px" height="128px">
+        </div>
+        <h2 class="aideus-left">Prospects</h2>
+        <p class="aideus-desc">Modern science is inconceivable without computers. And although computers and intelligent systems essentially improve scientific research efficiency, they neither make a scientist cleverer nor do they allow them to increase conceptual complexity of resolved problems. Namely this is required for solution of the most perspective problems which, inter alia, include medical and biologic researches, in particular, regarding unlimited increase of length of human life, economic research for abatement of the threat of world crises, research in the field of physics which could lead to creation of interstellar travel technology, etc. Methods of strong intelligence could be the necessary means for achievement of breakthrough in all areas. After all, such results as, for example, the discovery of penicillin which rescued millions of lives, are achieved too seldom due to insufficient efficiency of human reasoning. Even partial solution of strong artificial intelligence problem would allow humans to considerably accelerate discoveries and inventions which could be necessary for survival of all mankind.</p>
+      </div>
+      <div class="col-md-4">
+        <div class="aideus-icon">
+          <img src="" alt="" class="img-rounded" width="128px" height="128px">
+        </div>
+        <h2 class="aideus-left">Your assistance</h2>
+        <p class="aideus-desc">Now our project needs all possible assistance and support. We are at the stage of fundamental scientific research, while commercial application of results is possible only in the long term. If you are really excited with the idea of creation of a strong artificial intelligence, if you consider this goal noble and want to change the life of people to the better, please contact us, we are always glad to get acquainted with you.</p>
+      </div>
+    </div>
+  </div>
+@stop
---
+++ resources/views/templates/messages.blade.php
@@ -0,0 +1,3 @@
+@if (Session::has('info'))
+    <div class="alert alert-success" role="alert">{{ Session::get('info') }}</div>
+@endif
